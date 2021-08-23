e998e519f029065e297f98c1a4300f7e09e1a509
Caio R. Vieira <caio.robs98@gmail.com>
2021-08-23 12:41:42 -0300

    adiciona calculadora bitcoins-reais quase funcionando

---
+++ bitcoins-reais.py
@@ -0,0 +1,27 @@
+while(True):
+    print("1- Bitcoins para reais")
+    print("2- Reais para bitcoins")
+    print("3- Fechar\n")
+    option = input("Escolha uma opção: ")
+
+    if option.isnumeric():
+        operation = int(option)
+
+        if (operation == 1):
+            bitcoins = float(input("\nvalor em bitcoins: "))
+            reais = bitcoins/2
+            print(f"\n{bitcoins} bitcoins = {reais} reais\n")
+
+        elif (operation == 2):
+            reais = float(input("\nvalor em reais: "))
+            bitcoins = reais*2
+            print(f"\n{reais} reais = {bitcoins} bitcoins\n")
+
+        elif (operation == 3):
+            print("\nFechando aplicação...")
+            break
+
+        else:
+            print("\nOpção inválida\n")
+    else:
+        print("\nOpção inválida\n")
