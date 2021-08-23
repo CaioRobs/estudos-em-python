a92023d0cbe77f1c936de6af608a78fdb738fd86
Caio R. Vieira <caio.robs98@gmail.com>
2021-08-23 13:04:19 -0300

    fix accepted chars print location

--- bitcoins-reais.py
+++ bitcoins-reais.py
@@ -8,18 +8,19 @@ while(True):
 
     if option.isnumeric():
         operation = int(option)
-        print("\n[SOMENTE NUMEROS E .]")
 
         if (operation == 3):
             print("\nFechando aplicação...")
             break
 
         if (operation == 1):
+            print("\n[SOMENTE NUMEROS E .]")
             bitcoins = float(input("Insira valor em bitcoins: "))
             reais = bitcoins * exchange_rate
             print(f"\n{bitcoins} bitcoins = R$ {reais:.3f}\n")
 
         elif (operation == 2):
+            print("\n[SOMENTE NUMEROS E .]")
             reais = float(input("Insira valor em reais: "))
             bitcoins = reais / exchange_rate
             print(f"\nR$ {reais} = {bitcoins:.4f} bitcoins\n")
