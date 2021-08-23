baed0860328a1dd8525df438e940ba632955ee58
Caio R. Vieira <caio.robs98@gmail.com>
2021-08-23 13:19:50 -0300

    adiciona arquivos de estudo sobre contador

---
+++ contador.py
@@ -0,0 +1,16 @@
+cont = 0
+while True:
+    cont += 1
+    if (cont % 2 == 0):
+        continue
+    if (cont > 9):
+        break
+    print(cont, end=';')
+
+condicao = 0
+while condicao:
+    print(condicao)
+
+condicao = 1
+while condicao:
+    print(condicao)
