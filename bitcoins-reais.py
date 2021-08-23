c3f74151a76a6deb54a754b5835f0e202929d466
Caio R. Vieira <caio.robs98@gmail.com>
2021-08-23 13:01:17 -0300

    makes better ux

--- bitcoins-reais.py
+++ bitcoins-reais.py
@@ -1,27 +1,30 @@
+exchange_rate = 267323.27
+
 while(True):
-    print("1- Bitcoins para reais")
+    print("\n1- Bitcoins para reais")
     print("2- Reais para bitcoins")
     print("3- Fechar\n")
     option = input("Escolha uma opção: ")
 
     if option.isnumeric():
         operation = int(option)
+        print("\n[SOMENTE NUMEROS E .]")
 
-        if (operation == 1):
-            bitcoins = float(input("\nvalor em bitcoins: "))
-            reais = bitcoins * 267323.27
-            print(f"\n{bitcoins} bitcoins = {reais} reais\n")
-
-        elif (operation == 2):
-            reais = float(input("\nvalor em reais: "))
-            bitcoins = reais / 267323.27
-            print(f"\n{reais} reais = {bitcoins} bitcoins\n")
-
-        elif (operation == 3):
+        if (operation == 3):
             print("\nFechando aplicação...")
             break
 
+        if (operation == 1):
+            bitcoins = float(input("Insira valor em bitcoins: "))
+            reais = bitcoins * exchange_rate
+            print(f"\n{bitcoins} bitcoins = R$ {reais:.3f}\n")
+
+        elif (operation == 2):
+            reais = float(input("Insira valor em reais: "))
+            bitcoins = reais / exchange_rate
+            print(f"\nR$ {reais} = {bitcoins:.4f} bitcoins\n")
+
         else:
-            print("\nOpção inválida\n")
+            print("\nOpção inválida, tente novamente")
     else:
-        print("\nOpção inválida\n")
+        print("\nOpção inválida, tente novamente")
