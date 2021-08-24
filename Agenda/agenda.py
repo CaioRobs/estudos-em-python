28679658107debb0fc5bc6825a3069f737f9788c
Caio R. Vieira <caio.robs98@gmail.com>
2021-08-24 14:34:21 -0300

    adiciona, busca e apaga prontos, opçao de atualizar contato iniciada

--- Agenda/agenda.py
+++ Agenda/agenda.py
@@ -6,10 +6,14 @@ class Agenda():
         return Contato(nome, telefone, email, twitter, instagram)
 
     def buscar(contatos, nome):
-        for contato in contatos:
-            if contato.getNome() == nome:
-                print(f'{contato.getNome()} | {contato.getTelefone()} | {contato.getEmail()} | {contato.getTwitter()} | {contato.getInstagram()}')
-                break
+        if len(contatos) != 0:
+            for contato in contatos:
+                if contato.getNome() == nome:
+                    print(f'{contato.getNome()} | {contato.getTelefone()} | {contato.getEmail()} | {contato.getTwitter()} | {contato.getInstagram()}')
+                    return
+            print('Nome não encontrado!\n')
+        else:
+            print('Lista está vazia!')
 
     def deletarContato(contatos, nome):
         if len(contatos) != 0:
@@ -17,9 +21,17 @@ class Agenda():
             for contato in contatos:
                 if contato.getNome() == nome:
                     contatos.pop(contador)
-                    return 'Contado {nome} removido com sucesso!'
-                else:
-                    return 'Nome não encontrado!'
+                    print(f'Contato {nome} removido com sucesso!')
+                    return
                 contador += 1
+            print('Nome não encontrado!')
+        else:
+            print('Lista está vazia!')
+
+    def atualizar(contatos, nome):
+        if len(contatos) != 0:
+            for contato in contatos:
+                if contato.getNome() == nome:
+                    print(f'{contato} será atualizado')
         else:
             return 'Lista está vazia!'
--- Agenda/main.py
+++ Agenda/main.py
@@ -7,10 +7,11 @@ print('~'*30)
 contatos = []
 
 while True:
+    print('Selecione uma opção:')
     print('Cadastrar contato -> 1')
     print('Buscar contato -> 2')
     print('Apagar contato -> 3')
-    print('Listar contatos -> 4')
+    print('Atualizar contato -> 4')
     print('Apagar todos os contatos -> 5')
     print('Sair -> 6\n')
     option = int(input('Selecione uma opção: '))
@@ -23,16 +24,23 @@ while True:
         twitter = input('Digite o twitter: ')
         instagram = input('Digite o instagram: ')
         contatos.append(Agenda.adicionar(nome, telefone, email, twitter, instagram))
+        print('\nContato adicionado')
         print('~'*30+'\n')
 
     elif option == 2:
         nome = input('Digite o nome do contato: ')
-        print('\n')
+        print()
         Agenda.buscar(contatos, nome)
         print('~'*30+'\n')
 
     elif option == 3:
         nome = input('Digite o nome do contato a deletar: ')
-        print(Agenda.deletarContato(contatos, nome))
+        print()
+        resultado = Agenda.deletarContato(contatos, nome)
         print('~'*30+'\n')
-  
+
+    elif option == 4:
+        nome = input('Digite o nome do contato a atualizar: ')
+        print()
+        Agenda.atualizar(contatos, nome)
+
