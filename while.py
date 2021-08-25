7ba8f6c250afeb29825528ea38fb73de2ccbdfa9
Caio R. Vieira <caio.robs98@gmail.com>
2021-08-25 13:51:35 -0300

    estudos do dia 25/08

---
+++ while.py
@@ -0,0 +1,15 @@
+
+# loop infinito
+# while 1 > 0:
+#     b = int(input('aqe'))
+#     while True:
+#         pontos = 0
+#         b += 1
+#         print(b)
+
+contador = 1
+while contador < 20:
+    contador += 1
+    while contador < 10:
+        print('entre')
+    print('não entre')
